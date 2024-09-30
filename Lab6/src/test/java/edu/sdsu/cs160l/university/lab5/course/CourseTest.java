01ee0e72f9823762f572741a2376cefb33bc88e4
jpateros <jpateros5410@sdsu.edu>
2024-09-29 17:48:44 -0700

    uploaded lab6 material

---
+++ Lab6/src/test/java/edu/sdsu/cs160l/university/lab5/course/CourseTest.java
@@ -0,0 +1,41 @@
+package edu.sdsu.cs160l.university.lab5.course;
+
+import org.junit.jupiter.api.Test;
+
+import edu.sdsu.cs160l.university.lab5.course.Course;
+import edu.sdsu.cs160l.university.lab5.course.CourseFactory;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CourseTest {
+
+    //TODO: Comment out these test cases and run the unit tests once you have implemnetd the singleton classes for CS 150 - CS340
+    @Test
+    public void testInstanceCS150(){
+//        Course c1 = CS150.getInstance();
+//        Course c2 = CS150.getInstance();
+//        assertEquals(c1, c2);
+    }
+
+    @Test
+    public void testInstanceCS160(){
+//        Course c1 = CS160.getInstance();
+//        Course c2 = CS160.getInstance();
+//        assertEquals(c1, c2);
+    }
+
+    @Test
+    public void testInstanceCS210(){
+//        Course c1 = CS210.getInstance();
+//        Course c2 = CS210.getInstance();
+//        assertEquals(c1, c2);
+    }
+
+    @Test
+    public void testInstance340(){
+//        Course c1 = CS340.getInstance();
+//        Course c2 = CS340.getInstance();
+//        assertEquals(c1, c2);
+    }
+
+}
